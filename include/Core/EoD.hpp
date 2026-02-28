61d43674f52c0dbaf9233b4e19bc08962f183968
wyrexdev <uchihasasukedev@gmail.com>
2026-02-28 22:40:23 +0300

    feat(eod): add Record struct and update zone to use ankerl::unordered_dense::map

--- include/Core/EoD.hpp
+++ include/Core/EoD.hpp
@@ -15,6 +15,7 @@
 
 #include <thread>
 
+#include <ankerl/unordered_dense.h>
 #include <unordered_map>
 
 class EoD
@@ -43,6 +44,15 @@ public:
     };
 
     std::unordered_map<int, Thread> activeThreads;
+
+    struct Record
+    {
+        uint16_t type;
+        uint32_t ttl;
+        std::vector<uint8_t> rdata;
+    };
+
+    ankerl::unordered_dense::map<std::string, std::vector<Record>> zone;
 
     void write16(std::vector<uint8_t> &buf, uint16_t value)
     {
